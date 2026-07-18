d1aa9a11fb28ed58e26c9c04b696c3e93cfb5979
Prince <prince.sindhu@visiongroupretail.com>
2026-07-18 14:58:20 +0530

    Initial Commit

---
+++ SnapIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SnapIt
+//
+//  Created by Prince on 18/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SnapIt/SnapItApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapItApp.swift
+//  SnapIt
+//
+//  Created by Prince on 18/07/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
